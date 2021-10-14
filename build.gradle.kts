06a407a6ee3629a61266852826d6859825a3b6c2
David Groomes <dwgroomes@gmail.com>
2021-10-13 23:45:01 -0500

    Add SLF4J

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,9 +6,13 @@ repositories {
     mavenCentral()
 }
 
+val slf4jVersion = "1.7.32" // releases: http://www.slf4j.org/news.html
 val graphqlJavaVersion = "17.3" // releases: https://github.com/graphql-java/graphql-java/releases
 
 dependencies {
+    implementation("org.slf4j:slf4j-api:$slf4jVersion")
+    runtimeOnly("org.slf4j:slf4j-simple:$slf4jVersion")
+
     implementation("com.graphql-java:graphql-java:$graphqlJavaVersion")
 }
 
--- src/main/java/dgroomes/App.java
+++ src/main/java/dgroomes/App.java
@@ -1,8 +1,14 @@
 package dgroomes;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class App {
 
+    private static final Logger log = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
-        System.out.println("Not yet implemented!");
+        log.info("Received the following arguments: {}", String.join(", ", args));
+        log.error("Not yet implemented!");
     }
 }
