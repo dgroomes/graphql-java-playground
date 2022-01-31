7181e827063b962b72f489f348cc12c1dda3acd3
David Groomes <dwgroomes@gmail.com>
2022-01-30 21:51:57 -0600

    Add the '@gp_uppercase' directive (not functional)

--- custom-directives/README.md
+++ custom-directives/README.md
@@ -13,7 +13,10 @@ official docs to begin your learning journey with GraphQL directives. Don't be a
 itself! Section [3.13 Directives](https://spec.graphql.org/October2021/#sec-Type-System.Directives) describes directives
 in clear detail.
 
-Custom directives can be defined for use in the schema, in queries, or both!
+Custom directives can be defined for use in the schema, in queries, or both! In this project, we define a directive that
+can be used in queries for upper-casing strings: `@gp_uppercase`. Per convention, the directive is given a custom prefix
+ending in `_` so that users can distinguish it from standard built-in directives like `@skip` and `@include`. In this
+case, `gp` is the acronym of `graphql-playground.`
 
 ## Instructions
 
@@ -29,14 +32,18 @@ Follow these instructions to build and run the app:
         forestAnimals(count: 3) @gp_uppercase
       }'
       ```
-    * You should notice that a list of three forest animals was printed to the screen and they are all upper-cased!
+    * You should notice that a list of three forest animals was printed to the screen and they are all upper-cased! It
+      should look something like the following.
+    * ```text
+      [main] INFO dgroomes.GraphqlUtil - {forestAnimals=[DEER, BEAR, OWL]}
+      ```
 4. Alias the build and run commands for happier development:
     * `alias go="./gradlew installDist && build/install/custom-directives/bin/custom-directives"`
     * For example, try the following command to build and run the program in one short step.
     * ```bash
       go ' 
       {
-        forestAnimals(count: 3) @gp_uppercase
+        forestAnimals(count: 4) @gp_uppercase
       }'
       ```
     * Next, try omitting the `@gp_uppercase` directive and see what happens.  
--- custom-directives/src/main/java/dgroomes/CustomDirectives.java
+++ custom-directives/src/main/java/dgroomes/CustomDirectives.java
@@ -1,13 +1,20 @@
 package dgroomes;
 
 import graphql.GraphQL;
+import graphql.schema.GraphQLDirective;
+import graphql.schema.GraphQLSchemaElement;
+import graphql.schema.GraphQLTypeVisitorStub;
+import graphql.schema.SchemaTransformer;
 import graphql.schema.idl.SchemaGenerator;
 import graphql.schema.idl.SchemaParser;
+import graphql.util.TraversalControl;
+import graphql.util.TraverserContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 
+import static graphql.introspection.Introspection.DirectiveLocation.FIELD;
 import static graphql.schema.idl.RuntimeWiring.newRuntimeWiring;
 
 /**
@@ -34,9 +41,33 @@ public class CustomDirectives {
 
         var schemaGenerator = new SchemaGenerator();
         var graphQLSchema = schemaGenerator.makeExecutableSchema(typeDefinitionRegistry, runtimeWiring);
+
+        // Further transform the schema after it was generated from the SDL file ("schema.graphqls"). Specifically,
+        // add the "@gp_uppercase" custom directive.
+        graphQLSchema = SchemaTransformer.transformSchema(graphQLSchema, new GraphQLTypeVisitorStub() {
+
+            @Override
+            public TraversalControl visitGraphQLDirective(GraphQLDirective node, TraverserContext<GraphQLSchemaElement> context) {
+                String nodeName = node.getName();
+                log.debug("Visiting directive '{}'", nodeName);
+                if ("gp_uppercase".equals(nodeName)) {
+                    return changeNode(context, uppercaseDirective());
+                }
+                return super.visitGraphQLDirective(node, context);
+            }
+        });
+
         var build = GraphQL.newGraphQL(graphQLSchema).build();
         var executionResult = build.execute(graphqlQuery);
 
         GraphqlUtil.printResult(executionResult);
+    }
+
+    public static GraphQLDirective uppercaseDirective() {
+        return GraphQLDirective.newDirective()
+                .name("gp_uppercase")
+                .description("WIP Directs the executor to uppercase strings.")
+                .validLocations(FIELD)
+                .build();
     }
 }
