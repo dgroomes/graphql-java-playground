f1775ad040fee453ab8cbbd2c033161eb719c92a
David Groomes <dwgroomes@gmail.com>
2022-12-29 00:28:12 -0600

    Use version catalog (+ TOML) in `instrumentation/`

--- instrumentation/build.gradle.kts
+++ instrumentation/build.gradle.kts
@@ -12,15 +12,11 @@ java {
     }
 }
 
-val slf4jVersion = "1.7.36" // SLF4J releases: http://www.slf4j.org/news.html
-val graphqlJavaVersion = "19.2" // GraphQL Java releases: https://github.com/graphql-java/graphql-java/releases
-val jacksonVersion = "2.13.4" // Jackson releases: https://github.com/FasterXML/jackson/wiki/Jackson-Releases
-
 dependencies {
-    implementation("org.slf4j:slf4j-api:$slf4jVersion")
-    runtimeOnly("org.slf4j:slf4j-simple:$slf4jVersion")
-    implementation("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
-    implementation("com.graphql-java:graphql-java:$graphqlJavaVersion")
+    implementation(libs.slf4j.api)
+    runtimeOnly(libs.slf4j.simple)
+    implementation(libs.jackson.databind)
+    implementation(libs.graphql.java)
 }
 
 application {
